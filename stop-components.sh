03a7d46114b4a31bc24232b745bb87c11b38913b
Trista Sie <tristasie@gmail.com>
2020-12-21 00:26:31 +0800

    將停止cc的指令放在stop-component

--- stop-components.sh
+++ stop-components.sh
@@ -4,6 +4,11 @@ printf "停止Kafka其他元件服務...\n"
 #進到confluent platform 5.5.1資料夾
 cd ~/confluent-5.5.1
 
+#停止control center
+~/Kafka_Services_Script/Each_Service/control-center-stop.sh &
+echo 等待Control Center關閉完成
+sleep 5
+
 #停止ksqldb
 ~/Kafka_Services_Script/Each_Service/ksqldb-stop.sh &
 
--- stop-major-services.sh
+++ stop-major-services.sh
@@ -9,11 +9,6 @@ cd ~/confluent-5.5.1
 echo 等待Schema Registry關閉完成
 sleep 5
 
-#停止control center
-~/Kafka_Services_Script/Each_Service/control-center-stop.sh &
-echo 等待Control Center關閉完成
-sleep 5
-
 #停止kafka
 ~/Kafka_Services_Script/Each_Service/kafka-stop.sh &
 echo 等待Kafka關閉完成
